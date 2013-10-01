c3e1a19c1c2928cbf6759afdf9339446c68d84c0
Dylan Cali <calid1984@gmail.com>
2013-10-01 16:59:31 -0500

    add zmq_version helper function

--- lib/ZMQ/FFI/Util.pm
+++ lib/ZMQ/FFI/Util.pm
@@ -10,20 +10,28 @@ use Sub::Exporter -setup => {
     exports => [qw(
         zcheck_error
         zcheck_null
+        zmq_version
     )],
 };
 
 my $zmq_errno = FFI::Raw->new(
-    'libzmq.so',
-    'zmq_errno',
-    FFI::Raw::int
+    'libzmq.so' => 'zmq_errno',
+    FFI::Raw::int # returns errno
+    # void
 );
 
 my $zmq_strerror = FFI::Raw->new(
-    'libzmq.so',
-    'zmq_strerror',
-    FFI::Raw::str,
-    FFI::Raw::int
+    'libzmq.so' => 'zmq_strerror',
+    FFI::Raw::str,  # returns error str
+    FFI::Raw::int   # errno
+);
+
+my $zmq_version = FFI::Raw->new(
+    'libzmq.so' => 'zmq_version',
+    FFI::Raw::void,
+    FFI::Raw::ptr,  # major
+    FFI::Raw::ptr,  # minor
+    FFI::Raw::ptr   # patch
 );
 
 sub zcheck_error {
@@ -46,6 +54,16 @@ sub zdie {
     my ($func) = @_;
 
     croak "$func: ".$zmq_strerror->($zmq_errno->());
+}
+
+sub zmq_version {
+    my ($major, $minor, $patch) = map { pack 'L!', $_ } (0, 0, 0);
+
+    my @ptrs = map { unpack('L!', pack('P', $_)) } ($major, $minor, $patch);
+
+    $zmq_version->(@ptrs);
+
+    return map { unpack 'L!', $_ } ($major, $minor, $patch);
 }
 
 1;
