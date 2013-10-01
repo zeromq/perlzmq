bc32277b9593a2435b6ee7335ce69fc5a414eb08
Dylan Cali <calid1984@gmail.com>
2013-10-01 16:15:28 -0500

    implement zmq send recv wrappers

--- lib/ZMQ/FFI/Socket.pm
+++ lib/ZMQ/FFI/Socket.pm
@@ -4,6 +4,7 @@ use Moo;
 use namespace::autoclean;
 
 use FFI::Raw;
+use Carp;
 
 use ZMQ::FFI::Util qw(zcheck_error zcheck_null);
 
@@ -17,7 +18,7 @@ has type => (
     required => 1,
 );
 
-has _socket => (
+has _socket_ptr => (
     is => 'rw',
 );
 
@@ -45,6 +46,46 @@ my $zmq_bind = FFI::Raw->new(
     FFI::Raw::str
 );
 
+my $zmq_send = FFI::Raw->new(
+    'libzmq.so',
+    'zmq_send',
+    FFI::Raw::int, # retval
+    FFI::Raw::ptr, # socket
+    FFI::Raw::str, # message
+    FFI::Raw::int, # length
+    FFI::Raw::int  # flags
+);
+
+my $zmq_msg_init = FFI::Raw->new(
+    'libzmq.so',
+    'zmq_msg_init',
+    FFI::Raw::int, # retval
+    FFI::Raw::ptr, # zmq_msg_t ptr
+);
+
+my $zmq_msg_recv = FFI::Raw->new(
+    'libzmq.so',
+    'zmq_msg_recv',
+    FFI::Raw::int, # retval
+    FFI::Raw::ptr, # msg ptr
+    FFI::Raw::ptr, # socket
+    FFI::Raw::int  # flags
+);
+
+my $zmq_msg_data = FFI::Raw->new(
+    'libzmq.so',
+    'zmq_msg_data',
+    FFI::Raw::ptr, # msg data ptr
+    FFI::Raw::ptr  # msg ptr
+);
+
+my $zmq_msg_close = FFI::Raw->new(
+    'libzmq.so',
+    'zmq_msg_data',
+    FFI::Raw::int, # retval
+    FFI::Raw::ptr  # msg ptr
+);
+
 my $zmq_close = FFI::Raw->new(
     'libzmq.so',
     'zmq_close',
@@ -52,30 +93,79 @@ my $zmq_close = FFI::Raw->new(
     FFI::Raw::ptr,
 );
 
+my $memcpy = FFI::Raw->new(
+    'libc.so.6',
+    'memcpy',
+    FFI::Raw::ptr,  # dest filled
+    FFI::Raw::ptr,  # dest buf
+    FFI::Raw::ptr,  # src
+    FFI::Raw::int   # buf size
+);
+
 sub BUILD {
     my $self = shift;
 
-    $self->_socket( $zmq_socket->($self->ctx_ptr, $self->type) );
+    $self->_socket_ptr( $zmq_socket->($self->ctx_ptr, $self->type) );
 
-    zcheck_null('zmq_socket', $self->_socket);
+    zcheck_null('zmq_socket', $self->_socket_ptr);
 }
 
 sub connect {
     my ($self, $endpoint) = @_;
 
-    zcheck_error('zmq_connect', $zmq_connect->($self->_socket, $endpoint));
+    unless ($endpoint) {
+        croak 'usage: $socket->connect($endpoint)'
+    }
+
+    zcheck_error('zmq_connect', $zmq_connect->($self->_socket_ptr, $endpoint));
 }
 
 sub bind {
     my ($self, $endpoint) = @_;
 
-    zcheck_error('zmq_bind', $zmq_bind->($endpoint));
+    unless ($endpoint) {
+        croak 'usage: $socket->bind($endpoint)'
+    }
+
+    zcheck_error('zmq_bind', $zmq_bind->($self->_socket_ptr, $endpoint));
+}
+
+sub send {
+    my ($self, $msg, $flags) = @_;
+
+    $flags //= 0;
+
+    zcheck_error(
+        'zmq_send', 
+        $zmq_send->($self->_socket_ptr, $msg, length($msg), $flags)
+    );
+}
+
+sub recv {
+    my ($self, $flags) = @_;
+
+    $flags //= 0;
+
+    my $msg_ptr = FFI::Raw::memptr(40); # large enough to hold zmq_msg_t
+
+    zcheck_error('zmq_msg_init', $zmq_msg_init->($msg_ptr));
+    
+    my $msg_size = $zmq_msg_recv->($msg_ptr, $self->_socket_ptr, $flags);
+    zcheck_error('zmq_msg_recv', $msg_size);
+
+    my $data_ptr    = $zmq_msg_data->($msg_ptr);
+    my $content_ptr = FFI::Raw::memptr($msg_size);
+
+    $memcpy->($content_ptr, $data_ptr, $msg_size);
+    $zmq_msg_close->($msg_ptr);
+
+    return $content_ptr->tostr($msg_size);
 }
 
 sub close {
     my $self = shift;
 
-    zcheck_error('zmq_close', $zmq_close->($self->_socket));
+    zcheck_error('zmq_close', $zmq_close->($self->_socket_ptr));
 }
 
 sub DEMOLISH {
