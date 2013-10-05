02e70142964cff8c78251c5178b3ff2c89dd0909
Dylan Cali <calid1984@gmail.com>
2013-10-05 03:59:20 -0500

    can't depend on linker_name (libzmq.so) symlink existing
    fallback to trying the version sonames (libzmq.so.3, libzmq.so.2) and if those
    don't exist die
    
    expose both a zmq_soname function and SONAME package variable that holds the
    soname being used

--- lib/ZMQ/FFI/Util.pm
+++ lib/ZMQ/FFI/Util.pm
@@ -7,34 +7,73 @@ use warnings;
 
 use FFI::Raw;
 use Carp;
+use Try::Tiny;
 
 use Sub::Exporter -setup => {
     exports => [qw(
+        zmq_soname
         zcheck_error
         zcheck_null
         zmq_version
     )],
 };
 
-my $zmq_errno = FFI::Raw->new(
-    'libzmq.so' => 'zmq_errno',
-    FFI::Raw::int # returns errno
-    # void
-);
+our $SONAME;
 
-my $zmq_strerror = FFI::Raw->new(
-    'libzmq.so' => 'zmq_strerror',
-    FFI::Raw::str,  # returns error str
-    FFI::Raw::int   # errno
-);
+my $zmq_errno;
+my $zmq_strerror;
+my $zmq_version;
 
-my $zmq_version = FFI::Raw->new(
-    'libzmq.so' => 'zmq_version',
-    FFI::Raw::void,
-    FFI::Raw::ptr,  # major
-    FFI::Raw::ptr,  # minor
-    FFI::Raw::ptr   # patch
-);
+BEGIN {
+    # .so symlink conventions are linker_name => soname => real_name
+    # e.g. libzmq.so => libzmq.so.X => libzmq.so.X.Y.Z
+    # Unfortunately not all distros follow this convention (Ubuntu).
+    # So first we'll try the linker_name, then the sonames, and then give up
+    my @sonames = qw(libzmq.so libzmq.so.3 libzmq.so.2);
+
+    FIND_SONAME:
+    for (@sonames) {
+        try {
+            $SONAME = $_;
+
+            $zmq_errno = FFI::Raw->new(
+                $SONAME => 'zmq_errno',
+                FFI::Raw::int # returns errno
+                # void
+            );
+
+            $zmq_strerror = FFI::Raw->new(
+                $SONAME => 'zmq_strerror',
+                FFI::Raw::str,  # returns error str
+                FFI::Raw::int   # errno
+            );
+
+            $zmq_version = FFI::Raw->new(
+                $SONAME => 'zmq_version',
+                FFI::Raw::void,
+                FFI::Raw::ptr,  # major
+                FFI::Raw::ptr,  # minor
+                FFI::Raw::ptr   # patch
+            );
+        }
+        catch {
+            undef $SONAME;
+        };
+
+        last FIND_SONAME if $SONAME;
+    }
+
+    unless ($SONAME) {
+        croak
+            q(Could not load libzmq, tried: ).
+            join(', ', @sonames)."\n".
+            q(Is libzmq on your ld path?);
+    }
+}
+
+sub zmq_soname {
+    return $SONAME;
+}
 
 sub zcheck_error {
     my ($func, $rc) = @_;
