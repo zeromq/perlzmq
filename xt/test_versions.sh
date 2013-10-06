d71f569a00ded2d2cd8643f7f3c4e4fe9a2b8f74
Dylan Cali <calid1984@gmail.com>
2013-10-06 05:53:34 -0500

    add logic so test_versions.sh script runs out of box both locally and on travis

--- xt/test_versions.sh
+++ xt/test_versions.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-repodir="$HOME/git"
+function travis_test {
+    echo -e "\nlibzmq ${1}.x"
+    sudo ln -svf /usr/lib/libzmq.so.$1 /usr/lib/libzmq.so
+    sudo ldconfig -v
+    prove -lvr t
+}
+
+function local_test {
+    echo -e "\nlibzmq ${1}.x"
+    export LD_LIBRARY_PATH="$HOME/git/zeromq$1-x/src/.libs"
+    prove -lvr t
+}
 
 for v in 2 3
 do
-    echo -e "\nlibzmq ${v}.x"
-
-    export LD_LIBRARY_PATH="$repodir/zeromq$v-x/src/.libs"
-    prove -lvr t
+    if [[ -n $TRAVIS ]]
+    then
+        travis_test $v
+    else
+        local_test $v
+    fi
 done
+
