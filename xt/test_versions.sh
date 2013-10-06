a79ff7684bb1033bd71de1c4f3ca9911ed599c77
Dylan Cali <calid1984@gmail.com>
2013-10-06 06:24:44 -0500

    add sanity check that we're using the expected test version change ld symlink

--- xt/test_versions.sh
+++ xt/test_versions.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
 function travis_test {
-    echo -e "\nlibzmq ${1}.x"
-    sudo ln -svf /usr/lib/libzmq.so.$1 /usr/lib/libzmq.so
+    sudo rm -f /usr/lib/libzmq.so /usr/lib/x86_64-linux-gnu/libzmq.so
+
+    case $1 in
+        2)
+            soname='libzmq.so.1'
+            sodir='/usr/lib'
+            ;;
+        3)
+            soname='libzmq.so.3'
+            sodir='/usr/lib/x86_64-linux-gnu'
+            ;;
+    esac
+
+    echo -e "\n$soname"
+
+    sudo ln -svf $soname $sodir/libzmq.so
     sudo ldconfig -v
+    ls -l $sodir
+
+    # sanity test
+    ver=$(perl -M'ZMQ::FFI::Util q(zmq_version)' -E 'say((zmq_version)[0])')
+    if [[ $ver != $1 ]];
+    then
+        echo "unexpected version $ver != $1"
+        exit 1
+    fi
+
     prove -lvr t
 }
 
