52ba50fb2c7fbec072c5204e11fbfd0a06f1f0fa
Dylan Cali <calid1984@gmail.com>
2016-01-08 00:22:03 -0500

    rework template generation code
    treat common socket api as just template and append it to the socket
    templates
    
    various naming and formatting refactor

--- scripts/gen_modules.pl
+++ scripts/gen_modules.pl
@@ -9,40 +9,36 @@ use Path::Class qw(file);
 
 my $tt = Template::Tiny->new();
 
-my @templates = (
+my @socket_templates = (
     file('inc/template/lib/ZMQ/FFI/ZMQ2/Socket.pm.tt'),
     file('inc/template/lib/ZMQ/FFI/ZMQ3/Socket.pm.tt')
 );
 
-my $common_ffi = file('inc/template/lib/ZMQ/FFI/Common/Socket.in');
+my $common_tt = file('inc/template/lib/ZMQ/FFI/Common/Socket.tt');
 
-my $closed_socket_check = <<'END';
-
-    if ($_[0]->socket_ptr == -1) {
+my $socket_check = q(if ($_[0]->socket_ptr == -1) {
         carp "Operation on closed socket";
         return;
-    }
-END
+    });
 
 my $vars = {
     date                => split("\n", scalar(qx{date -u})),
-    zmq_common_api      => scalar $common_ffi->slurp(),
-    closed_socket_check => $closed_socket_check,
+    closed_socket_check => $socket_check,
 };
 
-for my $template (@templates) {
-    my $target = "$template";
+for my $socket_tt (@socket_templates) {
+    my $target = "$socket_tt";
     $target =~ s{^inc/template/}{}g;
     $target =~ s{\.tt$}{}g;
     $target = file($target);
 
-    my $input = $template->slurp();
+    my $input = $socket_tt->slurp();
+    $input   .= $common_tt->slurp();
+
     my $output;
-
     $tt->process(\$input, $vars, \$output);
-    $tt->process(\$output, $vars, \$output);
 
-    say "Generating '$target'\n\tfrom '$template'\n\tusing $common_ffi";
+    say "Generating '$target' from templates";
     $target->spew($output);
 }
 
