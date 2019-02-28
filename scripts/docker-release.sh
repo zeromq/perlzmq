fe9d755d140cf44fa224315785d21faf59dd55b4
Dylan Cali <calid1984@gmail.com>
2019-02-28 14:12:15 -0500

    pass image version explicitly

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
 
+version=$1
+
+if [ -z "$version" ]; then
+    echo "docker-release.sh <version>"
+    exit 1
+fi
+
 docker build . \
-    -t calid/zmq-ffi-testenv:1.13 \
+    -t calid/zmq-ffi-testenv:$version \
     -t calid/zmq-ffi-testenv:latest \
     -t calid/zmq-ffi-testenv:ubuntu
 
-for t in 1.13 latest ubuntu; do
+for t in $version latest ubuntu; do
     docker push calid/zmq-ffi-testenv:$t
 done
